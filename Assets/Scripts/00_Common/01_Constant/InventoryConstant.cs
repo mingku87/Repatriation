5502322bad44a1484ce63a4cdb952047786453cb
mingku87 <127922214+mingku87@users.noreply.github.com>
2025-07-17 12:57:42 +0900

    줄바꿈, 띄어쓰기 수정

--- Assets/Scripts/00_Common/01_Constant/InventoryConstant.cs
+++ Assets/Scripts/00_Common/01_Constant/InventoryConstant.cs
@@ -5,13 +5,13 @@ public static class InventoryConstant
     public static int MaxQuickSlotCount = 6;
     public static readonly Dictionary<EquipmentPart, List<EquipmentSlot>> AllowedEquipmentSlotsPerPart = new()
     {
-        { EquipmentPart.Head,  new() { EquipmentSlot.Head } },
-        { EquipmentPart.Body,  new() { EquipmentSlot.Body } },
-        { EquipmentPart.Arms,  new() { EquipmentSlot.LeftArm, EquipmentSlot.RightArm } },
-        { EquipmentPart.Hands, new() { EquipmentSlot.LeftHand, EquipmentSlot.RightHand } },
-        { EquipmentPart.Bag,   new() { EquipmentSlot.Bag } },
-        { EquipmentPart.Knees, new() { EquipmentSlot.LeftKnee, EquipmentSlot.RightKnee } },
-        { EquipmentPart.Feet,  new() { EquipmentSlot.LeftFoot, EquipmentSlot.RightFoot } },
+        { EquipmentPart.Head,  new(){ EquipmentSlot.Head } },
+        { EquipmentPart.Body,  new(){ EquipmentSlot.Body } },
+        { EquipmentPart.Arms,  new(){ EquipmentSlot.LeftArm, EquipmentSlot.RightArm } },
+        { EquipmentPart.Hands, new(){ EquipmentSlot.LeftHand, EquipmentSlot.RightHand } },
+        { EquipmentPart.Bag,   new(){ EquipmentSlot.Bag } },
+        { EquipmentPart.Knees, new(){ EquipmentSlot.LeftKnee, EquipmentSlot.RightKnee } },
+        { EquipmentPart.Feet,  new(){ EquipmentSlot.LeftFoot, EquipmentSlot.RightFoot } },
     };
 }
 
--- Assets/Scripts/00_Common/01_Constant/PlayerConstant.cs
+++ Assets/Scripts/00_Common/01_Constant/PlayerConstant.cs
@@ -31,21 +31,69 @@ public class PlayerConstant
     public static string AnimatorFloatMoveY = "MoveY";
 
     public static Dictionary<Difficulty, PlayerSettings> Settings = new() {
-        { Difficulty.Easy, new PlayerSettings() {
-            maxStatus = new(){{ Status.HP, 100.0f }, { Status.Thirst, 100.0f }, { Status.Symptom, 100.0f },
-                { Status.Atk, 10.0f }, { Status.AtkRange, 1.0f }, { Status.Def, 5.0f }, { Status.Speed, 3.0f }, { Status.SightRange, 5.0f }},
+        { Difficulty.Easy, new PlayerSettings(){
+            maxStatus = new(){
+                { Status.HP, 100.0f },
+                { Status.Thirst, 100.0f },
+                { Status.Symptom, 100.0f },
+                { Status.Atk, 10.0f },
+                { Status.AtkRange, 1.0f },
+                { Status.Def, 5.0f },
+                { Status.Speed, 3.0f },
+                { Status.SightRange, 5.0f }
+            },
             thirstDecayRate = 0.5f,
-            symptomDecayRate = new(){{ 10, 1.0f }, { 20, 0.5f }, { 40, 0.33f }, { 60, 0.2f }, { 80, 0.12f }, { 100, 0.08f }},
+            symptomDecayRate = new(){
+                { 10.0f, 1.0f },
+                { 20.0f, 0.5f },
+                { 40.0f, 0.33f },
+                { 60.0f, 0.2f },
+                { 80.0f, 0.12f },
+                { 100.0f, 0.08f }
+            },
             healthDecayRateByThirst = 3.0f,
-            healthDecayRateBySymptomRate = new(){{ 0, 64 }, { 1, 32 }, { 5, 16 }, { 10, 8 }, { 20, 4 }, { 30, 2 }, { 60, 1 }, { 100, 0 }}
+            healthDecayRateBySymptomRate = new(){
+                { 0, 64 },
+                { 1, 32 },
+                { 5, 16 },
+                { 10, 8 },
+                { 20, 4 },
+                { 30, 2 },
+                { 60, 1 },
+                { 100, 0 }
+            }
         }},
-        { Difficulty.Hard, new PlayerSettings() {
-            maxStatus = new(){{ Status.HP, 100.0f }, { Status.Thirst, 100.0f }, { Status.Symptom, 100.0f },
-                { Status.Atk, 10.0f }, { Status.AtkRange, 1.0f }, { Status.Def, 5.0f }, { Status.Speed, 3.0f }, { Status.SightRange, 5.0f }},
+        { Difficulty.Hard, new PlayerSettings(){
+            maxStatus = new(){
+                { Status.HP, 100.0f },
+                { Status.Thirst, 100.0f },
+                { Status.Symptom, 100.0f },
+                { Status.Atk, 10.0f },
+                { Status.AtkRange, 1.0f },
+                { Status.Def, 5.0f },
+                { Status.Speed, 3.0f },
+                { Status.SightRange, 5.0f }
+            },
             thirstDecayRate = 0.5f,
-            symptomDecayRate = new(){{ 10, 1.0f }, { 20, 0.5f }, { 40, 0.33f }, { 60, 0.2f }, { 80, 0.12f }, { 100, 0.08f }},
+            symptomDecayRate = new(){
+                { 10.0f, 1.0f },
+                { 20.0f, 0.5f },
+                { 40.0f, 0.33f },
+                { 60.0f, 0.2f },
+                { 80.0f, 0.12f },
+                { 100.0f, 0.08f }
+            },
             healthDecayRateByThirst = 3.0f,
-            healthDecayRateBySymptomRate = new(){{ 0, 64 }, { 1, 32 }, { 5, 16 }, { 10, 8 }, { 20, 4 }, { 30, 2 }, { 60, 1 }, { 100, 0 }}
+            healthDecayRateBySymptomRate = new(){
+                { 0, 64 },
+                { 1, 32 },
+                { 5, 16 },
+                { 10, 8 },
+                { 20, 4 },
+                { 30, 2 },
+                { 60, 1 },
+                { 100, 0 }
+            }
         }}
     };
 }
