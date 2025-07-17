46ca4618a9734d0389237cd515a24636300680e6
mingku87 <127922214+mingku87@users.noreply.github.com>
2025-07-17 09:14:43 +0900

    인벤토리 기초 작업

--- Assets/Scripts/01_Player/Inventory.cs
+++ Assets/Scripts/01_Player/Inventory.cs
@@ -2,13 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Inventory
+public class Inventory : SingletonObject<Inventory>
 {
     private List<Item> items;
+    private Dictionary<PlayerAction, Item> quickSlots;
+    private Item holdItem;
 
     public void Initialize()
     {
         items = new();
+        quickSlots = new() {
+            { PlayerAction.Slot1, null },
+            { PlayerAction.Slot2, null },
+            { PlayerAction.Slot3, null },
+            { PlayerAction.Slot4, null },
+            { PlayerAction.Slot5, null },
+            { PlayerAction.Slot6, null }
+        };
+        holdItem = null;
+    }
+
+    void Update()
+    {
+        UseItem();
+        CheckHoldItem();
+    }
+
+    public void UseItem()
+    {
+        if (Input.GetKeyDown(KeySetting.GetKey(PlayerAction.UseItem)) == false) return;
+        if (holdItem == null) return;
+
+        holdItem.Use();
+    }
+
+    public void CheckHoldItem()
+    {
+        foreach (var kv in quickSlots)
+        {
+            if (Input.GetKeyDown(KeySetting.GetKey(kv.Key)))
+            {
+                holdItem = kv.Value;
+                break;
+            }
+        }
     }
 
     public void AddItem(Item item)
--- Assets/Scripts/98_System/KeyManager.cs
+++ Assets/Scripts/98_System/KeyManager.cs
@@ -8,6 +8,7 @@ public enum PlayerAction
     MoveLeft,
     MoveRight,
     Interaction,
+    UseItem,
     Slot1,
     Slot2,
     Slot3,
@@ -38,7 +39,7 @@ public static class KeySetting
 public class KeyManager : SingletonObject<KeyManager>
 {
     private KeyCode[] defaultKeys = new KeyCode[]
-    { KeyCode.W, KeyCode.S, KeyCode.A, KeyCode.D, KeyCode.F,
+    { KeyCode.W, KeyCode.S, KeyCode.A, KeyCode.D, KeyCode.F, KeyCode.Mouse0,
       KeyCode.Alpha1, KeyCode.Alpha2, KeyCode.Alpha3, KeyCode.Alpha4, KeyCode.Alpha5, KeyCode.Alpha6 };
 
 
